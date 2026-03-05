7b8fb734e152f77b4e90bb07cdaba0f3e38552cf
Maria Eduarda Araujo <maria.eduarda.araujo1006@gmail.com>
2026-03-05 09:56:43 -0300

    fix: corrigindo problema de jar

--- src/main/java/com/lunarismailapi/service/MensagemService.java
+++ src/main/java/com/lunarismailapi/service/MensagemService.java
@@ -9,8 +9,8 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 @Service
 public class MensagemService {
@@ -20,19 +20,22 @@ public class MensagemService {
         this.mailSender = mailSender;
     }
     public void enviarEmail(Mensagem mensagem) throws MessagingException, IOException {
-        String html;
 
         ClassPathResource resource = new ClassPathResource("templates/emailTemplate.html");
-        html = Files.readString(resource.getFile().toPath(), StandardCharsets.UTF_8);
-        html = html.replace("${nomeAluno}", mensagem.getNomeAluno());
-        html = html.replace("${conteudo}", mensagem.getConteudo());
+        try (InputStream inputStream = resource.getInputStream()) {
+            String html = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
 
-        MimeMessage email = mailSender.createMimeMessage();
-        MimeMessageHelper mensagemHp = new MimeMessageHelper(email);
+            html = html.replace("${nomeAluno}", mensagem.getNomeAluno());
+            html = html.replace("${conteudo}", mensagem.getConteudo());
 
-        mensagemHp.setTo(mensagem.getEmailProfessor());
-        mensagemHp.setSubject(mensagem.getAssunto() + " - " + mensagem.getNomeAluno());
-        mensagemHp.setText(html,true);
-        mailSender.send(email);
+            MimeMessage email = mailSender.createMimeMessage();
+            MimeMessageHelper mensagemHp = new MimeMessageHelper(email, "UTF-8");
+
+            mensagemHp.setTo(mensagem.getEmailProfessor());
+            mensagemHp.setSubject(mensagem.getAssunto() + " - " + mensagem.getNomeAluno());
+            mensagemHp.setText(html, true);
+
+            mailSender.send(email);
+        }
     }
 }
